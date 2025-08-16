6ea44be14e375c1ef0e9356db12407f3ecfb2ee9
doolster <zrdooley@gmail.com>
2025-08-15 20:02:33 -0400

    Small talk info fixes. Fall talks now being sorted correctly.

--- OldSiteConversion/talkConverter.py
+++ OldSiteConversion/talkConverter.py
@@ -10,8 +10,8 @@ def formatURLs(str):
 
 monthDict = { 'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12' }
 
-for file in os.listdir('./temp/speakersWorkfiles'):
-    with open('./temp/speakersWorkfiles/' + file, encoding='utf8') as f:
+for file in os.listdir('./OldSiteConversion/speakersWorkfiles'):
+    with open('./OldSiteConversion/speakersWorkfiles/' + file, encoding='utf8') as f:
 #with open('./temp/speakersWorkfiles/Fall2018.txt', encoding='utf8') as f:
         term = file[:-8] + ' ' + file[-8:-4]
         lines = f.readlines()
@@ -19,7 +19,7 @@ for file in os.listdir('./temp/speakersWorkfiles'):
         output = []
         i = 0
         while i < len(lines):
-            with open('./temp/NewTalkInfo/' + (lines[i+1].strip() + '-' + lines[i].strip()).replace(' ', '') + '.txt', 'w', encoding='utf8') as f:
+            with open('./OldSiteConversion/NewTalkInfo/' + (lines[i+1].strip() + '-' + lines[i].strip()).replace(' ', '') + '.txt', 'w', encoding='utf8') as f:
                 f.write('Term: ' + term + '\n')
                 f.write('Date: ' + lines[i])
                 i += 1
